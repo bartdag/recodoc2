fb6528f4944d76673614d7fdc55a4623a205d3bd
Barthelemy Dagenais <bart@cs.mcgill.ca>
2011-03-23 17:44:58 -0400

    finished porting test. refs #1

--- recodoc2/apps/project/tests.py
+++ recodoc2/apps/project/tests.py
@@ -2,9 +2,13 @@ from __future__ import unicode_literals
 import os
 from django.test import TestCase
 from django.conf import settings
+from django.db import transaction
 from docutil.test_util import clean_test_dir
 from project.actions import create_project_local, create_project_db,\
-                            list_projects_db, list_projects_local
+                            list_projects_db, list_projects_local,\
+                            create_release_db
+
+from project.models import Project, ProjectRelease
 
 
 class ProjectTest(TestCase):
@@ -19,3 +23,58 @@ class ProjectTest(TestCase):
         create_project_local('project1')
         create_project_local('project2')
         self.assertEqual(2, len(os.listdir(settings.PROJECT_FS_ROOT_TEST)))
+
+    def testCreateProjectDB(self):
+        create_project_db('Project 1', 'http://www.example1.com', 'project1')
+        create_project_db('Project 2', 'http://www.example1.com', 'project2')
+        self.assertEqual(2, Project.objects.count())
+        for project in Project.objects.all():
+            project.delete()
+
+    def testCreateProjectVersion(self):
+        create_project_db('Project 1', 'http://www.example1.com', 'project1')
+        create_release_db('project1', '3.0', True)
+        create_release_db('project1', '3.1')
+        self.assertTrue(2, ProjectRelease.objects.count())
+        for version in ProjectRelease.objects.all():
+            version.delete()
+
+        for project in Project.objects.all():
+            project.delete()
+
+    def testCreateProjectDBNonUnique(self):
+        create_project_db('Project 1', 'http://www.example1.com', 'project1')
+
+        try:
+            sid = transaction.savepoint()
+            create_project_db('Project 1', 'http://www.example1.com',
+                              'project2')
+            self.fail('Should have failed!')
+        except:
+            transaction.savepoint_rollback(sid)
+            self.assertTrue(True)
+
+        try:
+            sid = transaction.savepoint()
+            create_project_db('Project 2', 'http://www.example1.com',
+                              'project1')
+            self.fail('Should have failed!')
+        except:
+            transaction.savepoint_rollback(sid)
+            self.assertTrue(True)
+
+        self.assertEqual(1, Project.objects.count())
+        for project in Project.objects.all():
+            project.delete()
+
+    def testListProjectsLocal(self):
+        create_project_local('project1')
+        create_project_local('project2')
+        self.assertEqual(2, len(list_projects_local()))
+
+    def testListProjectsDB(self):
+        create_project_db('Project 1', 'http://www.example1.com', 'project1')
+        create_project_db('Project 2', 'http://www.example1.com', 'project2')
+        self.assertEqual(2, len(list_projects_db()))
+        for project in Project.objects.all():
+            project.delete()
