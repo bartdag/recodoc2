eb8f319f1f0cd8e94db5592b1252a058ca925f58
Barthelemy Dagenais <bart@cs.mcgill.ca>
2011-03-29 06:39:59 -0400

    fix for new gitli issues...

--- recodoc2/apps/codebase/models.py
+++ recodoc2/apps/codebase/models.py
@@ -1,3 +1,48 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+from project.models import Project, ProjectRelease, SourceElement
 
-# Create your models here.
+
+LANGUAGES = [
+    ('j','Java'),
+    ('p','Python'),
+    ('pr','Properties'),
+    ('x','XML'),
+    ('jx','Java Stack Trace'),
+    ('l','Log Trace'),
+    ('r','Previous Message'),
+    ('o','Other'),
+]
+'''Language of an element'''
+
+SOURCE_TYPE = (
+    ('d','Document'),
+    ('s','Support Channel'),
+)
+
+
+def add_language(lang_code, lang_name):
+    global LANGUAGES
+    found = False
+    for (code, _) in LANGUAGES:
+        if lang_code == code:
+            found = True
+            break
+    if not found:
+        LANGUAGES.append((lang_code, lang_name))
+
+
+class CodeBase(models.Model):
+    '''A codebase includes all the code of a project release'''
+    
+    project = models.ForeignKey(Project)
+    '''att.'''
+    name = models.CharField(max_length=500, null=True, blank=True)
+    '''att.'''
+    project_release = models.ForeignKey(ProjectRelease)
+    '''att.'''
+    
+    def __unicode__(self):
+        return self.name + ' - ' + self.project.name + ' ' + self.project_release.release
