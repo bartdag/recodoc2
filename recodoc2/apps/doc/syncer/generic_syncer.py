5b9c8205e5ea6a472ba0e378bf670d70353df443
Barthelemy Dagenais <bart@cs.mcgill.ca>
2011-04-06 18:02:41 -0400

    more work on the doc syncer. refs #3

--- recodoc2/apps/doc/syncer/generic_syncer.py
+++ recodoc2/apps/doc/syncer/generic_syncer.py
@@ -1,15 +1,19 @@
 from __future__ import unicode_literals
 import urllib2
+import urlparse
 import logging
 from lxml import etree
-from url_util import get_local_url, get_url_without_hash, ensure_path_exists,\
-        get_path_from_url
+from docutil.url_util import get_local_url, get_url_without_hash,\
+        ensure_path_exists, get_path_from_url, get_sanitized_url
+from docutil.commands_util import get_encoding, download_file
 from doc.models import DocumentPage, DocumentLink
 
+
 class GenericSyncer(object):
-    '''Generic Documentation Syncer responsible for downloading all the pages of a document.
+    '''Generic Documentation Syncer responsible for downloading all the pages
+       of a document.
     '''
-    
+
     links = etree.XPath("//a")
     imgs = etree.XPath("//img")
     logger = logging.getLogger("docs.syncer.GenericSyncer")
@@ -36,17 +40,17 @@ class GenericSyncer(object):
             self.prefix_avoid = []
         else:
             self.prefix_avoid = prefix_avoid
-        
+
         if suffix_avoid is None:
             self.suffix_avoid = []
         else:
             self.suffix_avoid = suffix_avoid
-        
+
         self.pages = {}
 
     def sync(self):
         '''Main method of the syncer.
-        
+
         :rtype: A dict of GenericPage indexed by their local url.
         '''
         for input_url in self.input_urls:
@@ -55,41 +59,45 @@ class GenericSyncer(object):
                 self.pages[page.local_url] = page
                 self.process_links(page)
             except:
-                local_url = get_local_url(self.output_url,
-                        get_url_without_hash(input_url))
-                self.logger.exception(
-                    'This page could not be downloaded: {0} in {1}'.format(
-                        input_url, local_url))
-                error_page = DocumentPage(input, None, [])
-                self.pages[local_url] = error_page
-        
+                self.page_error(input_url, self.pages)
         return self.pages
+
+    def page_error(self, input_url, pages):
+        local_url = get_local_url(self.output_url,
+                get_url_without_hash(input_url))
+        self.logger.exception(
+            'This page could not be downloaded: {0} in {1}'.format(
+                input_url, local_url))
+        error_page = DocumentPage(input, None, [])
+        pages[local_url] = error_page
 
     def process_page(self, url):
         self.logger.info("Processing page: " + url)
         local_url = self.make_copy(get_url_without_hash(url))
 
-        parser = etree.HTMLParser()
         local_page = urllib2.urlopen(local_url)
+        parser = etree.HTMLParser(encoding=get_encoding(local_page))
         tree = etree.parse(local_page, parser)
 
         links = self.process_page_links(tree, local_url, url)
         self.process_page_imgs(tree, url)
-        
+
         local_page.close()
         page = DocumentPage(url, local_url, links)
-        
+
         return page
 
     def make_copy(self, url_to_copy, binary=False):
-        # Will probably need something more intelligent. Will do for now.
-        #destination = urlparse.urljoin(self.input_url, url_to_copy)
         destination_url = get_local_url(self.output_url, url_to_copy)
         try:
             ensure_path_exists(destination_url)
         except:
-            raise Exception('Could not make copy of %s in %s' % (url_to_copy, destination_url))
-        download_file(url_to_copy, get_path_from_url(destination_url),force=False,binary=binary)
+            raise Exception('Could not make copy of {0} in {1}'.format(
+                url_to_copy, destination_url))
+
+        download_file(url_to_copy, get_path_from_url(destination_url),
+                force=False, binary=binary)
+
         return destination_url
 
     def process_links(self, page):
@@ -98,36 +106,34 @@ class GenericSyncer(object):
         '''
         for link in page.links:
             if not self.in_scope(link):
-                continue;
+                continue
             elif not (link.local_url in self.pages):
                 try:
                     page = self.process_page(link.url)
                     self.pages[page.local_url] = page
                     self.process_links(page)
                 except:
-                    local_url = get_local_url(self.output_url, get_url_without_hash(link.url))
-                    self.logger.exception('This page could not be downloaded: %s in %s' % (link.url, local_url))
-                    error_page = GenericPage(link.url, None, [])
-                    self.pages[local_url] = error_page
+                    self.page_error(link.url, self.pages)
 
     def _should_avoid(self, link):
         should_avoid = False
-        
+
         for avoid in self.prefix_avoid:
             if link.startswith(avoid):
                 should_avoid = True
                 break
-            
+
         if not should_avoid:
             for avoid in self.suffix_avoid:
                 if link.endswith(avoid):
                     should_avoid = True
                     break
-        
+
         return should_avoid
 
     def in_scope(self, link):
-        in_scope = len(self.scope) == 0
+        # We don't want to make a google of ourselves...
+        in_scope = len(self.scope) != 0
         url = link.url
 
         for scope_item in self.scope:
@@ -135,7 +141,7 @@ class GenericSyncer(object):
                 if self._should_avoid(url):
                     in_scope = False
                     break
-                else: 
+                else:
                     in_scope = True
                     break
 
@@ -151,7 +157,7 @@ class GenericSyncer(object):
                 href = attributes['href']
                 link_url = get_url_without_hash(urlparse.urljoin(url, href))
                 local_url_to = get_sanitized_url(get_local_url(self.output_url, link_url))
-                link = GenericLink(link_url, local_url_to)
+                link = DocumentLink(link_url, local_url_to)
                 links.append(link)
             else:
                 continue
